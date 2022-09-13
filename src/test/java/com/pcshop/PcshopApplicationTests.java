c49e86a4c3d940e9f60a1f73440a3893f585c103
BrunodevOliveira <brunodevoliveira@gmail.com>
2022-09-13 12:15:27 -0300

    projeto iniciado

---
+++ src/test/java/com/pcshop/PcshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pcshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
